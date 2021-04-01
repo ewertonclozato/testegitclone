98dbbdac826656c063ec7a61b88f33804f65d355
Ewerton Clozato <ewertonclozato@otmail.com>
2021-04-01 06:57:37 -0300

    Teste GIT CLONE

---
+++ testegitclone.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá Mundo");
+        Console.WriteLine("Ultima versão");
+        Console.WriteLine("Mais um VERSIONAMENTO de código pelo GIT");
+        Console.WriteLine("Editando para fazer o PULL");
+        Console.WriteLine("Teste GIT CLONE");
+        Console.ReadKey();
+    }
+}
